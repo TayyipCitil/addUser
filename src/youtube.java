21b29d0ac63baf5a8e2b2d86dfed49bfe2ec37c0
= <=>
2024-03-08 22:45:01 +0300

    version1

---
+++ src/youtube.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class youtube {
+    //Kullanıcıdan veri alma
+    public static void main(String[] args) {
+        Scanner name = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String n;
+        n = name.nextLine();
+        System.out.println(n);
+
+    }
+}
